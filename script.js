7be155b100e039e3edc20257daa898d7f3275470
Radioserk <boujahdachraf@gmail.com>
2024-11-02 19:07:22 +0100

    Js update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-        function openModal() {
-            document.getElementById("taskModal").classList.remove("hidden");
-        }
+// Array to store tasks
+let tasks = [];
 
-        function closeModal() {
-            document.getElementById("taskModal").classList.add("hidden");
-        }
+// Modal open and close
+function openModal() {
+    document.getElementById("taskModal").classList.remove("hidden");
+}
 
-        
+function closeModal() {
+    document.getElementById("taskModal").classList.add("hidden");
+}
+
