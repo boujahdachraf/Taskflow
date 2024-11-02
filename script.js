f15392e0a33ce5941c80434622ebfce7afff08f3
Radioserk <boujahdachraf@gmail.com>
2024-11-02 19:09:31 +0100

    save task

--- script.js
+++ script.js
@@ -9,4 +9,24 @@ function openModal() {
 function closeModal() {
     document.getElementById("taskModal").classList.add("hidden");
 }
+// Save task functiion
+function saveTask() {
+    const title = document.getElementById("taskTitle").value;
+    const desc = document.getElementById("taskDesc").value;
+    const status = document.getElementById("taskStatus").value;
+    const dueDate = document.getElementById("taskDueDate").value;
+    const priority = document.getElementById("taskPriority").value;
+
+    if (title === "" || desc === "" || dueDate === "") {
+        alert("Please enter the title, description, and due date.");
+        return;
+    }
+
+    tasks.push({ title, desc, status, dueDate, priority });
+    clearInputs();
+    displayTasks();
+    closeModal();
+}
+
+
 
